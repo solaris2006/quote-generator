325608c7fa2c6f228e0ec4cce9c9072f3373bc5c
solaris2006 <cristian.stefan@gmail.com>
2021-12-27 14:23:47 +0200

    add js code for fetching quotes from api

--- script.js
+++ script.js
@@ -1,1 +1,52 @@
-console.log('Hello');
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
+let apiQuotes = [];
+
+// Show new quote
+
+function newQuote() {
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+
+  if (!quote.author) {
+    authorText.textContent = 'Unknown';
+  } else {
+    authorText.textContent = quote.author;
+  }
+
+  if (quote.text.length > 50) {
+    quoteText.classList.add('long-quote');
+  } else {
+    quoteText.classList.remove('long-quote');
+  }
+
+  quoteText.textContent = quote.text;
+}
+
+//  Get Quotes form Api
+
+async function getQuotes() {
+  const apiUrl = 'https://type.fit/api/quotes';
+
+  try {
+    const response = await fetch(apiUrl);
+    apiQuotes = await response.json();
+    newQuote();
+  } catch (error) {
+    //  Catch error
+  }
+}
+
+//  Get Quotes localy
+
+// function newQuote() {
+//   const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
+//   console.log(quote);
+// }
+
+//  On load
+
+getQuotes();
