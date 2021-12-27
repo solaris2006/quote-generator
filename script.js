e46d78eb21d5878c293f894a4da6fb143b715073
solaris2006 <cristian.stefan@gmail.com>
2021-12-27 15:00:32 +0200

    add loader effect

--- script.js
+++ script.js
@@ -3,12 +3,28 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
 let apiQuotes = [];
+
+// Show Loadding
+
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+//  Hide Loading()
+
+function complete() {
+  quoteContainer.hidden = false;
+  loader.hidden = true;
+}
 
 // Show new quote
 
 function newQuote() {
+  loading();
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
 
   if (!quote.author) {
@@ -22,19 +38,22 @@ function newQuote() {
   } else {
     quoteText.classList.remove('long-quote');
   }
-
+  //  Set Quote, Hide Loader
   quoteText.textContent = quote.text;
+  complete();
 }
 
 //  Get Quotes form Api
 
 async function getQuotes() {
+  loading();
   const apiUrl = 'https://type.fit/api/quotes';
 
   try {
     const response = await fetch(apiUrl);
     apiQuotes = await response.json();
     newQuote();
+    complete();
   } catch (error) {
     //  Catch error
   }
@@ -60,5 +79,4 @@ newQuoteBtn.addEventListener('click', newQuote);
 twitterBtn.addEventListener('click', tweetQuote);
 
 //  On load
-
 getQuotes();
